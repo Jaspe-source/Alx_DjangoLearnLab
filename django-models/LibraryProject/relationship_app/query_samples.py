0e6e385bd94dc1f1ad10ebe7a376513ab55e5bf3
Jasper Otieno <jasperotieno1@gmail.com>
2025-09-07 22:59:41 +0300

    Add query samples for Author, Book, and Librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,19 +1,13 @@
-from relationship_app.models import Author, Book, Library, Librarian
-
-
-# Query all books by a specific author
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
-
+from .models import Author, Book, Librarian, Library
 
 # List all books in a library
-def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+def list_books_in_library(library):
+    return Book.objects.filter(library=library)
 
+# Query all books by a specific author
+def books_by_author(author):
+    return Book.objects.filter(author=author)
 
 # Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+def librarian_for_library(library):
+    return Librarian.objects.get(library=library)
