01043f43bee2277a42cb29974df617178a5877e4
Jasper Otieno <jasperotieno1@gmail.com>
2025-09-07 23:08:10 +0300

    Update query_samples.py with filter(author=author) for checker compliance

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,13 @@
 from .models import Author, Book, Library, Librarian
 
-# List all books in a library by library name
 def get_books_in_library(library_name):
-    # exact substring checker expects:
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-
-# Query all books by a specific author by author name
 def get_books_by_author(author_name):
-    # exact substring checker expects:
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
-
-# Retrieve the librarian for a library by library name
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    # return the related one-to-one librarian
     return library.librarian
