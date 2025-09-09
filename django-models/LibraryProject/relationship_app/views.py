231dc485ab1d874667d141f33d197ff6f32d37d4
Jasper Otieno <jasperotieno1@gmail.com>
2025-09-09 14:46:47 +0300

    Fixed template path for function-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Library
-from django.views.generic.detail import DetailView
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 
-# Function-based view: list all books
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view: show details of a specific library
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-
-    # Class-based view to display library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
