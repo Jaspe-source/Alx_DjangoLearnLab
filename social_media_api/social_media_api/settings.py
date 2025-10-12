c7faf4bed26c139acc5ed72049187d6c67c6d930
Jasper Otieno <jasperotieno1@gmail.com>
2025-10-12 13:27:25 +0300

    Add root API welcome endpoint

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from django.http import JsonResponse
 import os
 import dj_database_url
 from pathlib import Path
